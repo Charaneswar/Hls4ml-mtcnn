95279d46f42b3bebf1dc31948cf1746b945939d2
Charan Eswar <32614346+Charaneswar@users.noreply.github.com>
2020-02-02 23:31:29 +0530

    debug

--- hls4ml2/read.py
+++ hls4ml2/read.py
@@ -2,6 +2,7 @@ import numpy as np
 import yaml
 import h5py
 import json
+
 # /Users/charan/Desktop/elec.I/Project/Hls4ml-mtcnn/hls4ml2/keras_config.yml
 with open('/Users/charan/Desktop/elec.I/Project/Hls4ml-mtcnn/hls4ml2/keras_config.yml') as file:
     yamlConfig = yaml.full_load(file)
@@ -21,10 +22,18 @@ for keras_layer in model_arch["config"]["layers"]:
     layer = {}
 
     # Extract name for finding weights and biases
-    layer['name'] = keras_layer['name']
     # weights = f['/{}/{}/kernel:0']
     # .format(layer['name'], layer['name'])][()]
+    # print(layer['name'])
+    for config, config_value in keras_layer["config"].items():
+        if config == "activation":
+            layer['activation'] = config_value
+            # layer['name'] = keras_layer['name']
+
+            # print(layer['activation'])
+    layer['name'] = keras_layer['name']
     print(layer['name'])
+    print(keras_layer["config"].items())
 
 # List all groups
 # print("Keys: %s" % f.keys())
@@ -35,8 +44,9 @@ for keras_layer in model_arch["config"]["layers"]:
 # data = list(f[a_group_key]
 print(list(f.attrs.keys()))
 print(f.attrs['layer_names'])
+ju = 'max_pooling2d_7'
 # s = f.get('conv2d_13/conv2d_13')
-d = f.get('/conv2d_13/conv2d_13')
-e = list(d.items())
-r = np.array(d.get('kernel:0'))
-print(r.shape)
+d = f.get('/{}/{}'.format(ju, ju))
+# e = list(d.items())
+# r = np.array(d.get('kernel:0'))
+print(d)
